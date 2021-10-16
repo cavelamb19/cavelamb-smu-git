ed75533cbab2b97b9586f2de0b4621b469a770a6
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-10-16 20:07:08 +0800

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -4,10 +4,10 @@ from app import Employee, Learner
 
 class TestEmployee(unittest.TestCase):
     def testemployee(self):
-        employee = Employee(staffID = 1, Name = 'Phris Coskitt', Username = 'csok', Email = 'csoki@gmail.com', 
+        employee = Employee(StaffID = 1, Name = 'Phris Coskitt', Username = 'csok', Email = 'csoki@gmail.com', 
         CurrentDesignation = 'administrator', Department ='hr', ContactNo = '90227823')
         self.assertEqual(employee.to_dict(), {
-            'staffID' : 1,
+            'StaffID' : 1,
             'Name' : 'Phris Coskitt',
             'Username' : 'csok',
             'Email' : 'csoki@gmail.com',
@@ -18,10 +18,11 @@ class TestEmployee(unittest.TestCase):
 
 class Testlearner(unittest.TestCase):
     def testlearner(self):
-        learner = Learner( learnerID = 3, Name = 'Constance Wilkinson', Username = 'cons', Email = 'cons@gmail.com',
+        learner = Learner(StaffID = 3, Name = 'Constance Wilkinson', Username = 'cons', Email = 'cons@gmail.com',
         CurrentDesignation = 'learner', Department = 'Learning', ContactNo = '92130843', CoursesTaking = 'IS212', CompletedCourses = 'IS214')
         self.assertEqual(learner.to_dict(), {
-            'staffID' : 3,
+            'StaffID' : 3,
+            'id' : None,
             'Name' : 'Constance Wilkinson',
             'Username' : 'cons',
             'Email' : 'cons@gmail.com',
