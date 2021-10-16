53947704a741041f9cac764bf4749ea6465cbb92
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-10-16 17:58:31 +0800

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,12 +1,12 @@
 import unittest
-from app import Employee
+from app import Employee, Learner
 
 
 class TestEmployee(unittest.TestCase):
     def testemployee(self):
         employee = Employee(staffID = 1, Name = 'Phris Coskitt', Username = 'csok', Email = 'csoki@gmail.com', 
         CurrentDesignation = 'administrator', Department ='hr', ContactNo = '90227823')
-        self.assertEqual(employee.json(), {
+        self.assertEqual(employee.to_dict(), {
             'staffID' : 1,
             'Name' : 'Phris Coskitt',
             'Username' : 'csok',
@@ -15,6 +15,17 @@ class TestEmployee(unittest.TestCase):
             'Department' : 'hr',
             'ContactNo' : '90227823'
         })
+
+class Testlearner(unittest.TestCase):
+    def testlearner(self):
+        learner = Learner(Name = 'Constance Wilkinson', CoursesTaking = 'IS212', CompletedCourses = 'IS214')
+        self.assertEqual(learner.to_dict(), {
+            'Name' : 'Constance Wilkinson',
+            'CoursesTaking' : 'IS212',
+            'CompletedCourses' : 'IS214'
+        })
+
+
 
     
         
