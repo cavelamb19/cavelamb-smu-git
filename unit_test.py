5047598b5a25cdafc6a7136963f2b988e2794851
Namisus <berniceseah2000@gmail.com>
2021-10-16 20:26:08 +0800

    Trainer Test
    test to dict for trainer

--- unit_test.py
+++ unit_test.py
@@ -1,5 +1,5 @@
 import unittest
-from app import Employee, Learner
+from app import Employee, Learner, Trainer
 
 
 class TestEmployee(unittest.TestCase):
@@ -16,7 +16,7 @@ class TestEmployee(unittest.TestCase):
             'ContactNo' : '90227823'
         })
 
-class Testlearner(unittest.TestCase):
+class TestLearner(unittest.TestCase):
     def testlearner(self):
         learner = Learner(StaffID = 3, Name = 'Constance Wilkinson', Username = 'cons', Email = 'cons@gmail.com',
         CurrentDesignation = 'learner', Department = 'Learning', ContactNo = '92130843', CoursesTaking = 'IS212', CompletedCourses = 'IS214')
@@ -33,6 +33,23 @@ class Testlearner(unittest.TestCase):
             'CompletedCourses' : 'IS214'
             
         })
+
+class TestTrainer(unittest.TestCase):
+    def testtrainer(self):
+        trainer = Trainer(StaffID = 2, Name = 'Arnold de Mari', Username = 'Dr', Email = 'Dr@gmail.com',
+        CurrentDesignation = 'trainer', Department = 'Training', ContactNo = '82329832', coursesTeaching = 'IS212')
+        self.assertEqual(trainer.to_dict(), {
+            'StaffID' : 2,
+            'id' : None,
+            'Name' : 'Arnold de Mari',
+            'Username' : 'Dr',
+            'Email' : 'Dr@gmail.com',
+            'CurrentDesignation' : 'trainer',
+            'Department': 'Training',
+            'ContactNo' : '82329832',
+            'coursesTeaching' : 'IS212'
+        })
+
 
 
 
