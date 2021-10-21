40e11f5b48c262eff63f7bd891144850e265a348
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-10-21 22:24:57 +0800

    Update lms_IS212.sql

--- lms_IS212.sql
+++ lms_IS212.sql
@@ -49,7 +49,7 @@ CREATE TABLE `Classes` (
   `currentEnrolled` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `classes` (`classesID`, `startDate`, `startTime`, `endDate`, `endTime`, `classesSize`, `trainerAssigned`) VALUES
+INSERT INTO `classes` (`classesID`, `startDate`, `startTime`, `endDate`, `endTime`, `classesSize`, `trainerAssigned`, `currentEnrolled`) VALUES
 (1, '2021-10-18', '2021-10-18 06:30:00', '2021-11-18', '2021-11-18 06:30:00', 50, 'Arnold de Mari', 0);
 
 -- --------------------------------------------------------
