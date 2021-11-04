b1420898c67955a11a8d0094670a681909f595d3
qisheng97 <72507713+qisheng97@users.noreply.github.com>
2021-11-04 14:43:24 +0800

    update integration test

--- integration_tests.py
+++ integration_tests.py
@@ -20,6 +20,65 @@ class TestApp(flask_testing.TestCase):
         db.drop_all()
         
 
+class TestgetLearners(TestApp):
+    
+      def test_get_all_learners(self):
+          
+          learner1= Learner(
+              CompletedCourses="IS214 System Design", ContactNo="92130843", CoursesAssigned="",
+              CoursesEnrolled="", CurrentDesignation="Engineer", Department="Learning",
+              Email="constan@gmail.com", Name="Constance TAN", Role="learner",
+              StaffID=1, Username="cons", id=1)
+          
+          learner2 = Learner(
+              CompletedCourses="IS115 Computational Thinking", ContactNo="90247788", CoursesAssigned="",
+              CoursesEnrolled="", CurrentDesignation="Engineer", Department="Learning",
+              Email="johnson@gmail.com", Name="Johnson sim", Role="learner",
+              StaffID=2, Username="john", id=2)
+      
+          db.session.add(learner1)
+          db.session.add(learner2)
+          db.session.commit()
+          
+          response = self.client.get("/learner/")
+          
+          self.assertEqual.__self__.maxDiff = None
+          self.assertEqual(response.status_code, 200)
+          self.assertEqual(response.json, {'code': 200, 'data': {"course": [
+            {
+                "CompletedCourses": "IS214 System Design",
+                "ContactNo": "92130843",
+                "CoursesAssigned": "",
+                "CoursesEnrolled": "",
+                "CurrentDesignation": "Engineer",
+                "Department": "Learning",
+                "Email": "constan@gmail.com",
+                "Name": "Constance TAN",
+                "Role": "learner",
+                "StaffID": 1,
+                "Username": "cons",
+                "id": 1
+            },
+            {
+                "CompletedCourses": "IS115 Computational Thinking",
+                "ContactNo": "90247788",
+                "CoursesAssigned": "",
+                "CoursesEnrolled": "",
+                "CurrentDesignation": "Engineer",
+                "Department": "Learning",
+                "Email": "johnson@gmail.com",
+                "Name": "Johnson sim",
+                "Role": "learner",
+                "StaffID": 2,
+                "Username": "john",
+                "id": 2
+            }
+            
+          ] }})
+
+
+
+
 class TestSelfEnrolled(TestApp):
 
     def test_self_enrolled(self):
@@ -30,13 +89,20 @@ class TestSelfEnrolled(TestApp):
             Email="constan@gmail.com", Name="Constance TAN", Role="learner",
             StaffID=1, Username="cons", id=1)
 
+        course = Course(
+            courseID=1, courseName="IS212 Software Project Management G2", courseDesc="agile methods", preRequisites="NULL",classesID=1)
+        
+        classes = Classes(classesID=1, startDate="August 18 2021", startTime="8am",
+                          endDate="November 5 2021", endTime="12pm", classesSize=40, trainerAssigned="Roger Ng", currentEnrolled=0)
 
         db.session.add(learner)
+        db.session.add(course)
+        db.session.add(classes)
         db.session.commit()
        
         request_body = {
             'learnerid': learner.StaffID,
-            'enrolledCourses': "IS212 Software Project Management G2"
+            'enrolledCourses': course.courseName
             
          }
 
@@ -90,13 +156,22 @@ class TestWithdrawSelfEnrolled(TestApp):
             CoursesEnrolled="", CurrentDesignation="Engineer", Department="Learning",
             Email="constan@gmail.com", Name="Constance TAN", Role="learner",
             StaffID=1, Username="cons", id=1)
+        
+        course = Course(
+            courseID=1, courseName="IS212 Software Project Management G2", courseDesc="agile methods", preRequisites="NULL", classesID=1)
+
+        classes = Classes(classesID=1, startDate="August 18 2021", startTime="8am",
+                          endDate="November 5 2021", endTime="12pm", classesSize=40, trainerAssigned="Roger Ng", currentEnrolled=0)
+
 
         db.session.add(learner)
+        db.session.add(course)
+        db.session.add(classes)
         db.session.commit()
 
         request_body = {
             'learnerid': learner.StaffID,
-            'enrolledCourses': "IS212 Software Project Management G2"
+            'enrolledCourses': course.courseName
 
         }
 
@@ -123,7 +198,13 @@ class TestWithdrawSelfEnrolled(TestApp):
 class TestAssignLearner(TestApp):
 
     def test_assign_learner(self):
+        
+        course = Course(
+            courseID=1, courseName="IS212 Software Project Management G2", courseDesc="agile methods", preRequisites="NULL", classesID=1)
 
+        classes = Classes(classesID=1, startDate="August 18 2021", startTime="8am",
+                          endDate="November 5 2021", endTime="12pm", classesSize=40, trainerAssigned="Roger Ng", currentEnrolled=0)
+        
         learner = Learner(
             CompletedCourses="IS214 System Design", ContactNo="92130843", CoursesAssigned="",
             CoursesEnrolled="", CurrentDesignation="Engineer", Department="Learning",
@@ -131,11 +212,13 @@ class TestAssignLearner(TestApp):
             StaffID=1, Username="cons", id=1)
 
         db.session.add(learner)
+        db.session.add(classes)
+        db.session.add(course)
         db.session.commit()
 
         request_body = {
             'learnerid': learner.StaffID,
-            'AssignedCourses': "IS212 Software Project Management G2"
+            'AssignedCourses': course.courseName
 
         }
 
@@ -160,10 +243,14 @@ class TestCreateQuizInfo(TestApp):
     
       def test_create_quiz_info(self):
           
+          course = Course(
+              courseID=1, courseName="IS212 Software Project Management G2", courseDesc="agile methods", preRequisites="NULL", classesID=1)
+
           classes = Classes(classesID=1, startDate="August 18 2021", startTime="8am",
                             endDate="November 5 2021", endTime="12pm", classesSize=40, trainerAssigned="Roger Ng", currentEnrolled=0)
-          lesson = Lesson(lessonID=1, courseMaterial="week1a introduction",classesID=1)
-          
+          lesson = Lesson(lessonID=1, courseMaterial="week1a introduction",classesID=classes.classesID)
+         
+          db.session.add(course)
           db.session.add(classes)
           db.session.add(lesson)
           db.session.commit()
@@ -198,12 +285,28 @@ class TestCreateQuizInfo(TestApp):
 class TestCreateTrueFalseQuestion(TestApp):
       
       def test_create_True_False_question(self):
-        
           
+          course = Course(
+              courseID=1, courseName="IS212 Software Project Management G2", courseDesc="agile methods", preRequisites="NULL", classesID=1)
+
+        
+          classes = Classes(classesID=1, startDate="August 18 2021", startTime="8am",
+                            endDate="November 5 2021", endTime="12pm", classesSize=40, trainerAssigned="Roger Ng", currentEnrolled=0)
+          lesson = Lesson(lessonID=1, courseMaterial="week1a introduction",classesID=1)
+          
+          quiz= Quiz(quizID=1,quizDuration="15 minutes", attemptNo="Unlimited", quizTitle="Week1 Quiz", quizDesc="No Calculator", lessonID=lesson.lessonID)
+         
+         
+          db.session.add(course)
+          db.session.add(classes)
+          db.session.add(lesson)
+          db.session.add(quiz)
+          db.session.commit()
+
           request_body = {
               'question': "Software Project Management module will teach you about agile process",
               'answer': "True",
-              'lessonID': 1,
+              'lessonID': quiz.lessonID,
               
           }
           
@@ -239,4 +342,54 @@ class TestCreateTrueFalseQuestion(TestApp):
                                                                      'qnID': 1,
                                                                      'quizID': 1}]})
               
+
+class TestAddlearnerscore(TestApp):
+    
+      def test_add_learner_score(self):
+          
+          course = Course(
+              courseID=1, courseName="IS212 Software Project Management G2", courseDesc="agile methods", preRequisites="NULL", classesID=1)
+
+          classes = Classes(classesID=1, startDate="August 18 2021", startTime="8am",
+                            endDate="November 5 2021", endTime="12pm", classesSize=40, trainerAssigned="Roger Ng", currentEnrolled=0)
+          lesson = Lesson(
+              lessonID=1, courseMaterial="week1a introduction", classesID=1)
+
+          quiz = Quiz(quizID=1, quizDuration="15 minutes", attemptNo="Unlimited",
+                      quizTitle="Week1 Quiz", quizDesc="No Calculator", lessonID=lesson.lessonID)
+          
+          learner = Learner(
+              CompletedCourses="IS214 System Design", ContactNo="92130843", CoursesAssigned="",
+              CoursesEnrolled="", CurrentDesignation="Engineer", Department="Learning",
+              Email="constan@gmail.com", Name="Constance TAN", Role="learner",
+              StaffID=1, Username="cons", id=1)
+
+
+          db.session.add(course)
+          db.session.add(classes)
+          db.session.add(lesson)
+          db.session.add(quiz)
+          db.session.add(learner)
+          db.session.commit()  
+          
+          request_body = {
+              'score': 2,
+              'quizID': quiz.quizID,
+              'learnerID': learner.StaffID,
+
+          }
+          
+          response = self.client.post("/addscore",
+                                      data=json.dumps(request_body),
+                                      content_type='application/json')
+
+          self.assertEqual(response.status_code, 200)
+          self.assertEqual(response.json, {"code": 200,"data": {
+                                                "qsID": 1,
+                                                "quizscore": 2,
+                                                "quizID": 1,
+                                                "learnerID": 1
+                                               
+                                                     }})
+
               
